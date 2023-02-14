9d3f30c0062be81f5436ca0c82960037301beb28
SuHlaingHnin <suhlaing.ucsy777@gmail.com>
2023-02-14 20:50:16 +0630

    Change Player Movement With Network Rigidbody

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,21 +1,59 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
     float movementSpeed = 3f;
+
+    public override void OnNetworkSpawn()
+    {
+        if(IsOwner)
+        {
+            Move();
+        }
+    }
 
     private void Update()
     {
         if (!IsOwner) return;
 
-        Vector3 moveDirection = new Vector3(0, 0, 0);
+        transform.position = Position.Value;
 
-        if (Input.GetKey(KeyCode.W)) moveDirection.z += 1f;
-        if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1f;
-        if (Input.GetKey(KeyCode.S)) moveDirection.z -= 1f;
-        if (Input.GetKey(KeyCode.D)) moveDirection.x += 1f;
+        //Vector3 moveDirection = new Vector3(0, 0, 0);
 
-        transform.position += moveDirection * movementSpeed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.W)) moveDirection.z += 1f;
+        //if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1f;
+        //if (Input.GetKey(KeyCode.S)) moveDirection.z -= 1f;
+        //if (Input.GetKey(KeyCode.D)) moveDirection.x += 1f;
+
+        //transform.position += moveDirection * movementSpeed * Time.deltaTime;
+    }
+
+    private void Move()
+    {
+        if(NetworkManager.Singleton.IsServer)
+        {
+            Vector3 randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        } else
+        {
+            SubmitPositionRequestServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    private void SubmitPositionRequestServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        Position.Value = GetRandomPositionOnPlane();
+    }
+
+    private Vector3 GetRandomPositionOnPlane()
+    {
+        return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
     }
 }
